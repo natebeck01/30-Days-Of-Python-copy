4a13e007fc1efca2fe9c7a1ff3bd08fdc0e5c04a
natebeck01 <nathanwbeckham@gmail.com>
2026-03-21 19:30:56 -0700

    Update variables.py

--- day_2/variables.py
+++ day_2/variables.py
@@ -4,3 +4,9 @@ full_name = 'Nathan Warren'
 is_married = False
 first_name, last_name, is_married = 'Nathan', 'Warren', False
 
+print(type('Nathan'))
+print(len(first_name))
+print(len(last_name))
+num_one = '5'
+num_two = '4'
+sum([num_one, num_two])
